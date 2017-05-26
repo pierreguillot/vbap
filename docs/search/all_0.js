d90cb1e9286c484919b5f7c93823c29b6c387f2b
pierreguillot <guillotpierre6@gmail.com>
2017-05-26 19:35:12 +0200

    update documentation

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['t_5fvbapd',['t_vbapd',['../group__vbapd.html#gad3ea4a0fc42d344bf172b328fcd9e7a2',1,'vbap.h']]],
   ['t_5fvbapf',['t_vbapf',['../group__vbapf.html#ga3c9b971298d6e8efed165328a6703ced',1,'vbap.h']]],
   ['todo_20list',['Todo List',['../todo.html',1,'']]]
 ];
--- docs/search/groups_0.js
+++ docs/search/groups_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['vbapd',['vbapd',['../group__vbapd.html',1,'']]],
   ['vbapf',['vbapf',['../group__vbapf.html',1,'']]]
 ];
